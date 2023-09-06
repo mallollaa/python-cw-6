545455e59b54b6319c0bd475b881c3bb758d1550
mallollaa <mah41411@gmail.com>
2023-09-06 22:24:24 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 # write your code here
+# --- part 1 -----
+person = {
+    'name' : 'manal',
+    'age' : 22,
+    "hobbies" : ["reading" , "playing" , "music"]
+}
+print(f" my name is {person['name']} I am {person['age']}")
+
+# ----- part 2 -----
+
+person["age"] = 33
+person['country'] = ' kuwait'
+
+print(person)
+print(len(person))
+
+# ---- part 3 ----
+
+person["hobbies"].append('watching tv')
+
+def check_hobbies(person):
+    if len(person['hobbies']) >= 8 :
+        print("WOW YOU ARE AMAZING")
+    else:
+        print("try harder")
+
+check_hobbies(person)
